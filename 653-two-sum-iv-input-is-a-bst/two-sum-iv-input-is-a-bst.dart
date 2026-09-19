d0288b6fe89936910cb5437a77cc3a92f4d9ed51
Hassibul Hossain Shanto <66798100+hassib2244@users.noreply.github.com>
2026-09-19 16:35:06 +0600

    Time: 7 ms (100.00%) | Memory: 154.6 MB (0.00%) - LeetSync

---
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.dart
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *   int val;
+ *   TreeNode? left;
+ *   TreeNode? right;
+ *   TreeNode([this.val = 0, this.left, this.right]);
+ * }
+ */
+class Solution {
+  bool findTarget(TreeNode? root, int k) {
+    final Set<int> seen = {};
+
+    bool dfs(TreeNode? node) {
+      if (node == null) return false;
+
+      int diff = k - node.val;
+
+      if (seen.contains(diff)) {
+        return true;
+      }
+
+      seen.add(node.val);
+
+      return dfs(node.left) || dfs(node.right);
+    }
+
+    return dfs(root);
+  }
+}
